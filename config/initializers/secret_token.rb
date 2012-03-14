15f391f0904d32c8a5b91547bec7d5b6fe45ecf9
unknown <rhino@rhino-PC.(none)>
2012-03-14 09:40:07 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '50aa91be76a5ed3bb43f6c9f6e71300ed4460e30e3973dacc474e0bf316bb60f977d28f6444fcc73fa17335b717f6efeb1fce51a304f5b5bec733362169f08da'
